a7bb506647c6cd8c8d6f6847ef668a9584cdc032
Rich <rjw12498@gmail.com>
2019-08-01 18:14:59 -0600

    Updates

---
+++ precache-manifest.44e79cef461ecad06a02b36b61a21838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effde8a09504a0c5f8568860cac527e3",
+    "url": "/clickerGame/index.html"
+  },
+  {
+    "revision": "ca003846d7c65d7a50d4",
+    "url": "/clickerGame/static/css/main.ff0a4e17.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickerGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "ca003846d7c65d7a50d4",
+    "url": "/clickerGame/static/js/main.809e98b3.chunk.js"
+  },
+  {
+    "revision": "b428fbe48bf4e28096bd",
+    "url": "/clickerGame/static/js/runtime~main.5bbc316d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickerGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
